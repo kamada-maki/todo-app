22f2c5e1cf719afe960e1555759d6cbc4a2c813c
kamada-maki <ikam.adamak17@gmail.com>
2021-02-25 21:44:45 +0900

    activehash修正

--- app/models/state.rb
+++ app/models/state.rb
@@ -2,7 +2,7 @@ class State < ActiveHash::Base
   self.data = [
     { id: 1, name: '--' },
     { id: 2, name: '未完了（進行前）' },
-    { id: 3, name: '未完了（進行中' },
+    { id: 3, name: '未完了（進行中）' },
     { id: 4, name: '完了' },
   ]
   include ActiveHash::Associations
