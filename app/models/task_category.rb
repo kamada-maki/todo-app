13e24c8d6ea86fffdbdba0123f9929a8fe4f9bed
kamada-maki <ikam.adamak17@gmail.com>
2021-02-25 14:19:38 +0900

    task_categoryにバリデーション記載

--- app/models/task_category.rb
+++ app/models/task_category.rb
@@ -1,3 +1,9 @@
 class TaskCategory < ApplicationRecord
   belongs_to :user
+  with_options presence: true do
+    validates :title
+    validates :description
+    validates :deadline
+    validates :state
+  end
 end
