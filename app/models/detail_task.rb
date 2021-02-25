3f41c49d72204f40330d42a4b848e629cd2ca7e0
kamada-maki <ikam.adamak17@gmail.com>
2021-02-25 17:51:29 +0900

    バリデーションの記述

--- app/models/detail_task.rb
+++ app/models/detail_task.rb
@@ -1,2 +1,13 @@
 class DetailTask < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :state
+  belongs_to :user
+  belongs_to :task_category
+  with_options presence: true do
+    validates :title
+    validates :description
+    validates :deadline
+  end
+  validates :user_id, presence: true 
+  validates :state_id, numericality: { other_than: 1 } 
 end
