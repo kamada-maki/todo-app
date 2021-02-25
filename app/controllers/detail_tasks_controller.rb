884d4c90bcf5ca7e71be239b666ccab621c35633
kamada-maki <ikam.adamak17@gmail.com>
2021-02-25 18:49:00 +0900

    コントローラーにcreateアクション記述

--- app/controllers/detail_tasks_controller.rb
+++ app/controllers/detail_tasks_controller.rb
@@ -1,2 +1,17 @@
 class DetailTasksController < ApplicationController
+  def create
+    @detail_task = DetailTask.new(detail_task_params)
+    if @detail_task.save
+      redirect_to "/task_categories/#{detail_task.task_category.id}"
+    else
+      @task_category = @detail_task.task_category
+      @detail_tasks = @task_category.detail_tasks
+      render "task_categories/show"
+    end
+  end
+  private
+  def detail_task_params
+    params.require(:detail_task)
+    .permit(:title, :description, :deadline, :state_id,:user_id).merge(user_id: current_user.id)
+  end
 end
