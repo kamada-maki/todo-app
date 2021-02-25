1e22896f36dcff3ebe23852337d1b8abc2f48196
kamada-maki <ikam.adamak17@gmail.com>
2021-02-25 18:49:43 +0900

    ネスト構造のルーティング記載

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'task_categories#index'
-  resources :task_categories
+  resources :task_categories do
+    resources :detail_tasks, only: [:create,:edit,:update]
+  end
 end
