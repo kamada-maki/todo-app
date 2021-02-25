249b9ed36d719b2248531b8a5659359145ef8bbb
kamada-maki <ikam.adamak17@gmail.com>
2021-02-25 14:26:10 +0900

    コントローラーにnew,createアクション記載

--- app/controllers/task_categories_controller.rb
+++ app/controllers/task_categories_controller.rb
@@ -1,4 +1,22 @@
 class TaskCategoriesController < ApplicationController
   def index
+    @task_categories = Task_category.all
+  end
+  def new
+    @task_category = Task_category.new
+  end
+  def create
+    @task_category=Task_category.new(task_category_params)
+      if @task_category.save
+        render :create
+      else
+        render :new
+      end
+  end
+  private
+
+  def task_category_params
+    params.require(:task_category)
+          .permit(:title, :description, :deadline, :state).merge(user_id: current_user.id)
   end
 end
