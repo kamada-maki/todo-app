b20be86450fb107b48652bcc0ce21a8398396eac
kamada-maki <ikam.adamak17@gmail.com>
2021-02-25 21:45:17 +0900

    detail_taskのnewをルーティングから削除

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'task_categories#index'
   resources :task_categories do
-    resources :detail_tasks, only: [:new,:create,:edit,:update]
+    resources :detail_tasks, only: [:create,:edit,:update]
   end
 end
